a23f1d15fd8e7ead4e58626cfee87177e1e41963
krof17 <kneumannakos@gmail.com>
2025-02-25 22:40:32 +0100

    Update live URLs

--- backend/server.js
+++ backend/server.js
@@ -52,8 +52,8 @@ app.post('/api/checkout', async (req, res) => {
       payment_method_types: ['card'],
       line_items: lineItems,
       mode: 'payment',
-      success_url: 'http://localhost:3000/success',
-      cancel_url: 'http://localhost:3000/products',
+      success_url: 'https://njmnn-frontend.vercel.app/success',
+      cancel_url: 'https://njmnn-frontend.vercel.app/products',
     });
 
     res.json({ id: session.id });
